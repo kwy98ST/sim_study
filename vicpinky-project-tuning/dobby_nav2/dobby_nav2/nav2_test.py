08561c2db4b5e013b8912fd17cba115b8a25fbfd
kwy98ST <kwy98ST@github.com>
2025-10-31 11:40:44 +0900

    second commit

--- vicpinky-project-tuning/dobby_nav2/dobby_nav2/nav2_test.py
+++ vicpinky-project-tuning/dobby_nav2/dobby_nav2/nav2_test.py
@@ -6,9 +6,9 @@ import math
 import tf_transformations
 
 
-def get_quaternion_from_yaw(yaw_radians):
-    """Converts a yaw angle in radians to a quaternion."""
-    quaternion = tf_transformations.quaternion_from_euler(0, 0, yaw_radians, axes='sxyz')
+def get_quaternion_from_yaw(yaw_degrees):
+    yaw_radians = math.radians(yaw_degrees)
+    quaternion = tf_transformations.quaternion_from_euler(0, 0, yaw_radians)
     return quaternion
 
 
@@ -16,33 +16,29 @@ def main(args=None):
     rclpy.init(args=args)
     nav2 = BasicNavigator()
     nav2.waitUntilNav2Active() 
-    initial_yaw_radians = -3.08823  # Yaw in radians
-    q = get_quaternion_from_yaw(initial_yaw_radians)
+    goal_yaw =  -3.08823
+    q = get_quaternion_from_yaw(goal_yaw)
     goal_pose = PoseStamped()
     goal_pose.header.frame_id = 'map'
     goal_pose.header.stamp = nav2.get_clock().now().to_msg()
-    goal_pose.pose.position.x = 0.0
-    goal_pose.pose.position.y = 0.0
-    goal_pose.pose.position.z = 0.0
-    goal_pose.pose.orientation.x = q[0]
-    goal_pose.pose.orientation.y = q[1]
-    goal_pose.pose.orientation.z = q[2]
-    goal_pose.pose.orientation.w = q[3]
-
-    nav2.get_logger().info(f'goal_pose: {goal_pose}')
-
-    nav2.setInitialPose(goal_pose)
-
     goal_pose.pose.position.x = -5.7914299964904785
     goal_pose.pose.position.y = -8.801392555236816
     goal_pose.pose.position.z = -0.001434326171875
+    goal_pose.pose.orientation.x = 0.0
+    goal_pose.pose.orientation.y = 0.0
+    goal_pose.pose.orientation.z = 0.0
+    goal_pose.pose.orientation.w = 1.0
 
-    nav2.get_logger().info(f'goal_pose: {goal_pose}')
+    nav2.setInitialPose(goal_pose)
+
+    goal_pose.pose.position.x = 0.0
+    goal_pose.pose.position.y = 0.0
+    goal_pose.pose.position.z = 0.0
 
     nav2.goToPose(goal_pose)
     while not nav2.isTaskComplete():
         feedback = nav2.getFeedback()
-        if feedback.navigation_duration > 600:
+        if feedback.navigation_time > 600:
             nav2.cancelTask()
 
         nav2.get_logger().info(f'feedback: {feedback}')
